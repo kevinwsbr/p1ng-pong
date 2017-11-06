ce6207cc3541d44d1ddebe8db56eeb4a955b3905
Rafael Monteiro Laranjeira <rml@ic.ufal.br>
2017-11-06 15:16:19 -0200

    Add files via upload

--- aaaa/aaaa.ino
+++ aaaa/aaaa.ino
@@ -6,15 +6,15 @@ int row = 5, column = 5;
 int dX = 1, dY = 1;
 int yp;
 int pont1=0,pont2=0;
-const int pinoPot = A5; //PINO ANALÓGICO UTILIZADO PELO POTENCIÔMETRO
-int leituraA5; //VARIÁVEL QUE ARMAZENA O VALOR LIDO NA PORTA ANALÓGICA
+const int pinoPot = A5;
+int leituraA5; 
 LedControl lc=LedControl(12,11,10,3);
 //LedControl lc=LedControl(11,13,10,3);
 
 void setup()
 {
-   pinMode(pinoPot, INPUT); //DEFINE A PORTA COMO ENTRADA
-  Serial.begin(9600); //INICIALIZA A SERIAL
+  pinMode(pinoPot, INPUT); 
+  Serial.begin(9600); 
   lc.shutdown(0,false);
   lc.shutdown(1,false);
   lc.shutdown(2,false);
@@ -31,13 +31,12 @@ void setup()
 void drawPad()
 {
   
-  yp = analogRead(pinoPot); //LÊ O VALOR NA PORTA ANALÓGICA (VALOR LIDO EM BITS QUE VAI DE 0 A 1023 BITS)
+  yp = analogRead(pinoPot); 
   yp=map(yp,100,1023,0,5);
-  Serial.print(leituraA5); //IMPRIME NA SERIAL O VALOR LIDO NA PORTA ANALÓGICA
-  Serial.println(yp); //IMPRIME O TEXTO NA SERIAL
+  Serial.print(leituraA5); 
+  Serial.println(yp); 
   Serial.println(pont2);
   Serial.println(pont2t);
-  //delay(100); //INTERVALO DE 100 MILISSEGUNDOS
   lc.setLed(0,7,yp,true);
   lc.setLed(0,7,yp+1,true);
   lc.setLed(0,7,yp+2,true);
@@ -52,15 +51,11 @@ void drawScore()
     lc.setLed(2,7,row,true);
   }
 
+
   for(i=0;i<pont2t;i++)
   {
+     if (pont2t>5) break;
     lc.setLed(2,pont2t,1,true);
-    if(i==5) 
-    {
-
-      break;
-    }
-    
   }
   for (int col = 7, row = 0; col >= 0; col--)
   {
@@ -93,9 +88,9 @@ void drawBall()
     }
   
     //if (row == 0 && dX == 1 ) {dX = -1;}
-    if (row == 0 && dX == 1 ) {tab = 1; row = 7; flag *= -1;}
+    if (row == -1 && dX == 1 ) {tab = 1; row = 7; flag *= -1;}
     if (row == 8 && dX == -1 ) {pont2++; newgame();}
-    if (row == 6 && dX == -1 && ( column==yp || column==yp+1 || column==yp+2 )) {dX=1;} 
+    if (row == 7 && dX == -1 && (column==yp || column==yp+1 || column==yp+2)) {dX=1;} 
     if (column == 0 && dY == -1 ) {dY = 1;}
     if (column == 7 && dY == 1 ) {dY = -1;}
   }else if(flag == -1)
@@ -119,12 +114,10 @@ void drawBall()
     }
   
     if (row == 0 && dX == 1 ) {dX = -1;}
-    if (row == 7 && dX == -1 ) {tab = 0; row = 0; flag *= -1;}
+    if (row == 8 && dX == -1 ) {tab = 0; row = 0; flag *= -1;}
     if (column == 0 && dY == -1 ) {dY = 1;}
     if (column == 7 && dY == 1 ) {dY = -1;}
   }
-  
-  
   delay(100);
   lc.clearDisplay(0);
   lc.clearDisplay(1);
@@ -134,7 +127,6 @@ void drawBall()
 void newgame()
 {
   pont2t++;
-  //lc.setLed(tab, row, column, true);
   lc.clearDisplay(0);
   row = 0;
   column= 5;
@@ -144,9 +136,9 @@ void newgame()
 
 void loop()
 {
-drawBall();
-drawScore();
- drawPad();  
+  drawBall();
+  drawScore();
+  drawPad();  
   /*for (int l = 0; l < 3; l++)
   {
     for (int col = 7; col >= 0; col--)
